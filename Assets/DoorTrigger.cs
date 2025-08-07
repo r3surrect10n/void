c579365850b2f92aa6ca6559d1bad3031fdae700
r3suurect10n <sosnin.a.a@yandex.ru>
2025-08-07 23:36:13 +0300

    add door trigger

---
+++ Assets/DoorTrigger.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField] private bool _doorStatus;
+
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerMovement>() && _doorStatus)
+            _animator.SetBool("IsOpen", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<PlayerMovement>())
+            _animator.SetBool("IsOpen", false);
+    }
+}
